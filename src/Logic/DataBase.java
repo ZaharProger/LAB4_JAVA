568ca218bdbd9c893f263b5a755778bba9b30b09
Захар Домолего <DomolegoZahar@gmail.com>
2021-11-28 12:57:04 +0800

    Добавил автмоатическое создание таблицы в классе БД

--- src/Logic/DataBase.java
+++ src/Logic/DataBase.java
@@ -20,6 +20,7 @@ public class DataBase {
         try{
             DriverManager.registerDriver(new JDBC());
             connection = DriverManager.getConnection(CONNECTION_ADDRESS_MASK + name);
+            create();
             result = "База данных успешно открыта!";
         }
         catch (SQLException exception){
@@ -29,14 +30,21 @@ public class DataBase {
         return result;
     }
 
+    private void create() throws SQLException{
+        try (Statement statement = connection.createStatement()){
+            statement.execute("CREATE TABLE IF NOT EXISTS 'students' ('id' INTEGER NOT NULL PRIMARY KEY, 'name' VARCHAR(50) NOT NULL, 'surname' VARCHAR(50) NOT NULL," +
+                                "'birthday' VARCHAR(10) NOT NULL, 'test_result' TINYINT NOT NULL, 'diff_result' TINYINT NOT NULL, 'exam_result' TINYINT NOT NULL);");
+        }
+    }
+
     public String executeAllData(){
         data.clear();
         String result;
         try (Statement statement = connection.createStatement()) {
             ResultSet executionResult = statement.executeQuery("SELECT * FROM students;");
             while (executionResult.next()){
-                data.add(new Student(executionResult.getInt("id"), executionResult.getString("student_name"), executionResult.getString("student_surname"),
-                        executionResult.getString("student_birthday"), executionResult.getBoolean("test_result"), executionResult.getByte("diff_result"),
+                data.add(new Student(executionResult.getInt("id"), executionResult.getString("name"), executionResult.getString("surname"),
+                        executionResult.getString("birthday"), executionResult.getBoolean("test_result"), executionResult.getByte("diff_result"),
                         executionResult.getByte("exam_result")));
             }
             result = (data.isEmpty())? "База данных пуста!" : "Чтение успешно завершено!";
