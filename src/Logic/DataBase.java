15a141d640149a7cb07137c2af66e68aca076f1e
Захар Домолего <DomolegoZahar@gmail.com>
2021-11-28 13:56:16 +0800

    Добавил добавление и удаление из БД

--- src/Logic/DataBase.java
+++ src/Logic/DataBase.java
@@ -32,8 +32,14 @@ public class DataBase {
 
     private void create() throws SQLException{
         try (Statement statement = connection.createStatement()){
-            statement.execute("CREATE TABLE IF NOT EXISTS 'students' ('id' INTEGER NOT NULL PRIMARY KEY, 'name' VARCHAR(50) NOT NULL, 'surname' VARCHAR(50) NOT NULL," +
-                                "'birthday' VARCHAR(10) NOT NULL, 'test_result' TINYINT NOT NULL, 'diff_result' TINYINT NOT NULL, 'exam_result' TINYINT NOT NULL);");
+            statement.execute("CREATE TABLE IF NOT EXISTS 'students' (" +
+                                    "'id' INTEGER NOT NULL PRIMARY KEY, " +
+                                    "'name' VARCHAR(50) NOT NULL, " +
+                                    "'surname' VARCHAR(50) NOT NULL, " +
+                                    "'birthday' VARCHAR(10) NOT NULL, " +
+                                    "'test_result' TINYINT NOT NULL, " +
+                                    "'diff_result' TINYINT NOT NULL, " +
+                                    "'exam_result' TINYINT NOT NULL);");
         }
     }
 
@@ -58,6 +64,40 @@ public class DataBase {
 
     public ArrayList<Student> getData(){
         return data;
+    }
+
+    public String addData(Student student){
+        String result;
+        try (Statement statement = connection.createStatement()){
+            statement.execute(String.format("INSERT INTO students ('id', 'name', 'surname', 'birthday', 'test_result', 'diff_result', 'exam_result')" +
+                    " VALUES (%d, '%s', '%s', '%s', %d, %d, %d);", student.getId(), student.getName(), student.getSurname(), student.getBirthday(),
+                    student.getTestResult()? 1 : 0, student.getDiffTestResult(), student.getExamResult()));
+            result = "Информация о студенте успешно добавлена!";
+        }
+        catch (SQLException exception){
+            result = exception.getMessage();
+        }
+
+        return result;
+    }
+
+    public String removeData(int id){
+        String result;
+        try (Statement statement = connection.createStatement()){
+            ResultSet executionResult = statement.executeQuery(String.format("SELECT id FROM students WHERE id = %d;", id));
+            if (executionResult.next()){
+                statement.execute(String.format("DELETE FROM students WHERE id = %d;", id));
+                result = "Информация о студенте успешно удалена!";
+            }
+            else{
+                result = "Информация о студенте не найдена!";
+            }
+        }
+        catch(SQLException exception){
+            result = exception.getMessage();
+        }
+
+        return result;
     }
 
     public String close(){
